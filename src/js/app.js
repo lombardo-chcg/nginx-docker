61d3d36d569327fe3556462f65e7c3b68c40525b
lombardo-chcg <dpa.lombardo@gmail.com>
2017-03-26 13:31:09 -0500

    add functor to js

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,37 @@
-function getGreetings() {
-  fetch('/api/greetings')
+const GREETINGS_ENDPOINT = '/api/greetings';
+
+async function getGreetings() {
+  const greetings = await makeApiCall(GREETINGS_ENDPOINT);
+
+  greetings
+    .map(data => createDivs(data))
+    .map(val => appendToDom(val, 'greetings'));
+}
+
+const Result = (val) => {
+  return {
+    map: (func) => func(val),
+    value: () => val
+  };
+}
+
+function makeApiCall(endpoint) {
+  return fetch(endpoint)
     .then(response => response.json())
-    .then(data => handleJsonArray(data))
+    .then(data => Result(data))
     .catch(error => console.log(error));
 }
 
-function setContentByDomID(content, id) {
-  document
-    .getElementById(id)
-    .innerHTML = content;
+function createDivs(jsonArray) {
+  const content = jsonArray
+    .map(node => `<div><h3>${node.language}: ${node.content}</h3></div>`)
+    .join('');
+
+  return Result(content);
 }
 
-function handleJsonArray(jsonArray) {
-  const content = jsonArray.map(node => node.content).join(' - ');
-
-  setContentByDomID(content, 'greetings');
+function appendToDom(content, DomId) {
+  document
+    .getElementById(DomId)
+    .innerHTML = content;
 }
